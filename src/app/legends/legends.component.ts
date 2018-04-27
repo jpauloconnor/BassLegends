cef58093e1ef19611a4a8513955f68197f165dd7
Paul O'Connor <jamespauloconnor@gmail.com>
2018-04-27 14:09:49 -0400

    List working

--- src/app/legends/legends.component.ts
+++ src/app/legends/legends.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Legend } from '../legend';
+import { LEGENDS } from '../mock-legends';
+
 
 @Component({
   selector: 'app-legends',
@@ -7,6 +9,8 @@ import { Legend } from '../legend';
   styleUrls: ['./legends.component.css']
 })
 export class LegendsComponent implements OnInit {
+  legends = LEGENDS;
+  
   legend: Legend = {
     id: 1,
     lastName: 'Wooten'
---
+++ src/app/mock-legends.ts
@@ -0,0 +1,14 @@
+import { Legend } from './legend';
+
+export const LEGENDS: Legend[] = [
+  { id: 1, lastName: 'Pastorius' },
+  { id: 2, lastName: 'Wooten' },
+  { id: 3, lastName: 'Jones' },
+  { id: 4, lastName: 'McCartney' },
+  { id: 5, lastName: 'Weymouth' },
+  { id: 6, lastName: 'Thunes' },
+  { id: 7, lastName: 'Collins' },
+  { id: 8, lastName: 'Flea' },
+  { id: 9, lastName: 'Claypool' },
+  { id: 10, lastName: 'Mingus' }
+];
